d5b1ce091450359bd1ced7e3dc410120c30a256c
Matt Klich <matt.klich@readytalk.com>
2016-02-06 20:56:44 -0700

    Initial Pelican config

---
+++ pelicanconf.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Matt Klich'
+SITENAME = u'Shell Abuse'
+SITESUBTITLE = u'Bending the shell to your whim - occasionally with abandon.'
+SITEURL = 'http://localhost:8000'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Denver'
+
+DEFAULT_LANG = u'en'
+
+FEED_DOMAIN = SITEURL
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Theme and theme config
+THEME = "Flex"
+SITELOGO = "//images/elementalvoid-avatar.png"
+DISQUS_SITENAME = "shellabuse"
+MAIN_MENU = True
+USE_FOLDER_AS_CATEGORY = True
+MENUITEMS = (
+    ('Archives', '/archives.html'),
+    ('Categories', '/categories.html'),
+    ('Tags', '/tags.html'),
+)
+# End theme and theme config
+
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2}T\d{2}:\d{2})_(?P<slug>.*)'
+
+EXTRA_PATH_METADATA = {
+    'images/favicon.ico': {'path': 'favicon.ico'},
+    'extra/CNAME': {'path': 'CNAME'},
+}
+
+TYPOGRIFY = True
+
+# Blogroll
+#LINKS = (
+#    ('Pelican', 'http://getpelican.com/'),
+#    ('Python.org', 'http://python.org/'),
+#    ('Jinja2', 'http://jinja.pocoo.org/'),
+#)
+
+
+# Social widget
+SOCIAL = (
+    ('linkedin', 'https://www.linkedin.com/in/mattklich'),
+    ('github', 'https://github.com/elementalvoid'),
+    ('google', 'https://google.com/+MattKlich'),
+    ('rss', '//shell-abuse.ninja/feeds/all.atom.xml')
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
