62f7b05559f7979dbce42bc9ca658b64cec1e65a
Matt Klich <mklich@shutterstock.com>
2018-11-26 00:44:05 -0700

    update pelican, theme, and switch to https

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://shell-abuse.ninja'
+SITEURL = 'https://shell-abuse.ninja'
 RELATIVE_URLS = False
 
 FEED_DOMAIN = SITEURL
